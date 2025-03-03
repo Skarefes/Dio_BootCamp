c8945aa2a625ef97479e0293f04093f1e17565fd
Roger Yamassaki <skarefes@gmail.com>
2025-03-02 21:02:06 -0300

    Terminando Contador

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -6,5 +6,31 @@ public class Contador {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
+
+        System.out.println("Digite o primeiro parametro:");
+        int parametro1 = teclado.nextInt();
+        System.out.println("Digite o segundo parametro:");
+        int parametro2 = teclado.nextInt();
+
+        try{
+            contar(parametro1, parametro2);
+       }catch (Exception e){
+            System.out.println("O segundo parametro deve ser maior que o primeiro");
+        }
+
+    }
+
+    static void contar(int parametro1, int parametro2) throws ParametrosInvalidosException{
+        if (parametro1 > parametro2){
+            throw new RuntimeException();
+        }
+
+        int contagem = parametro2 - parametro1;
+        for (int i = 1; i <= contagem
+                ; i ++){
+            System.out.println("Imprimindo o numero: " + i);
+        }
+
+
     }
 }
