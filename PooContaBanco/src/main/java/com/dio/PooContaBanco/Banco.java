a9fe469bbdc3630ee2c41fae4f0e789741ceb301
Roger Yamassaki <skarefes@gmail.com>
2025-03-14 20:00:15 -0300

    atualizacao

--- PooContaBanco/src/main/java/com/dio/PooContaBanco/Banco.java
+++ PooContaBanco/src/main/java/com/dio/PooContaBanco/Banco.java
@@ -3,6 +3,7 @@ package com.dio.PooContaBanco;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -11,7 +12,18 @@ public class Banco {
     private String nome;
     private List<Conta> contas;
 
+    public Banco(){
+        this.contas = new ArrayList<>();
+    }
 
+    public void addConta(Conta conta){
+        contas.add(conta);
+    }
 
-
+    public void listarClientes(){
+       if (contas == null){
+           System.out.println("Nenhuma conta criada");
+       }
+       System.out.println("Total de contas: " + contas.size());
+    }
 }
--- PooContaBanco/src/main/java/com/dio/PooContaBanco/PooContaBancoApplication.java
+++ PooContaBanco/src/main/java/com/dio/PooContaBanco/PooContaBancoApplication.java
@@ -13,17 +13,33 @@ public class PooContaBancoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		Banco bb = new Banco();
 		Cliente roger = new Cliente();
 		roger.setNome("Roger");
+		Cliente merlin = new Cliente();
+		merlin.setNome("Merlin");
 
 		Conta cc = new ContaCorrente(roger);
 		Conta cp = new ContaPoupanca(roger);
+		Conta cc2 = new ContaCorrente(merlin);
 
 		cc.deposito(200);
 		cc.transferencia(100, cp);
+		cc2.deposito(500);
+		cc2.saque(200);
 
+		bb.addConta(cc);
+		bb.addConta(cp);
+		bb.addConta(cc2);
+
+		System.out.println("***BANCO BB***");
+		bb.listarClientes();
+
+		System.out.println("---------");
 		cc.envioExtrato();
 		System.out.println("---------");
 		cp.envioExtrato();
+		System.out.println("---------");
+		cc2.envioExtrato();
 	}
 }
